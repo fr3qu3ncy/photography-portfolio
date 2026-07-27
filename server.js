b02a167de77792398f84a03444247404aeecd66c
Martin Sandford <martin@it2ms.com>
2026-07-27 18:34:32 +0100

    fix: serve uploads directory for thumbnails

--- server.js
+++ server.js
@@ -44,6 +44,7 @@ function saveData(data) {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static(CONFIG.uploadDir));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
